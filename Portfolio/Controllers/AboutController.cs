89279a751337ddc6e5e79efe982653068f045f6d
Yunus Yesilcimen <byunusyesilcimen@gmail.com>
2024-09-05 23:58:45 +0300

    Admin Panel Controller

---
+++ Portfolio/Controllers/AboutController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DL.Context;
+using Portfolio.DL.Entities;
+
+namespace Portfolio.Controllers
+{
+	public class AboutController : Controller
+	{
+		private readonly PortfolioContext _context;
+
+		public AboutController(PortfolioContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult AboutList()
+		{
+			var values = _context.Abouts.ToList();
+			return View(values);
+		}
+		[HttpGet]
+		public IActionResult CreateAbout()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateAbout(About about)
+		{
+			_context.Abouts.Add(about);
+			_context.SaveChanges();
+			return RedirectToAction("AboutList");
+		}
+
+		public IActionResult DeleteAbout(int id)
+		{
+			var value = _context.Abouts.Find(id);
+			_context.Abouts.Remove(value);
+			_context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
+
+		[HttpGet]
+		public IActionResult UpdateAbout(int id)
+		{
+			var value = _context.Abouts.Find(id);
+			return View(value);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateAbout(About about)
+		{
+			_context.Abouts.Update(about);
+			_context.SaveChanges();
+			return RedirectToAction("AboutList");
+		}
+	}
+}
--- Portfolio/Controllers/ExperienceController.cs
+++ Portfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DL.Context;
+using Portfolio.DL.Entities;
 
 namespace Portfolio.Controllers
 {
     public class ExperienceController : Controller
     {
-        public IActionResult ExperienceList()
+        private readonly PortfolioContext _context;
+
+		public ExperienceController(PortfolioContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult ExperienceList()
+        {
+            var values = _context.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id) 
+        {
+            var value = _context.Experiences.Find(id);
+            _context.Experiences.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
